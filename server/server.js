05a42be67118acfea4777ad329f393f36bfca6e5
Elvin Li Kam Wa <elvinlkw@hotmail.com>
2021-06-05 14:33:53 -0400

    fix bug where requests would time out

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
 // Routes
